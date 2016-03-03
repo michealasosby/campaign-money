94b94ba84b17bb8cfe24bcca5d2b571c825708a3
Micheala Sosby <mmsxv3@mail.missouri.edu>
2016-03-03 03:28:13 -0600

    commit
    commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+// Assignment:
+// Create a list of all of the donations to Kurt Schaefer's Campaign
+// (CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL)
+
+
+$(document).ready(function() {
+
+	// 1.
+	// When the page is loaded,
+	// call the loadData() function.
+
+});
+
+function loadData() {
+
+	// 2.
+	// Write an AJAX call here to load your data.
+	// Then PASS the data to writeTable();
+}
+
+
+function writeTable(data) {
+
+	// 3.
+	// Make a list of every donation made to "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL"
+	// Do this by looping through the data and writing a new table row (<tr></tr>) for every donation.
+	// Each row should contain of three columns (<td></td>): 
+	// - Contribution Date
+	// - Contributon Information
+	// - Amount
+}
+
