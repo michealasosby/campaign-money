71382b11c891c5354783b91b8fe2423612a7bce5
Micheala Sosby <mmsxv3@mail.missouri.edu>
2016-03-03 03:42:33 -0600

    commit
    commit

--- js/script.js
+++ js/script.js
@@ -5,14 +5,18 @@
 
 
 $(document).ready(function() {
-
+	loadData();
+});
 	// 1.
 	// When the page is loaded,
 	// call the loadData() function.
 
-});
+
 
 function loadData() {
+	$.getJSON("js/donations_over_5K.json", function (donationsData) {
+		writeTable(donationsData);
+	});
 
 	// 2.
 	// Write an AJAX call here to load your data.
@@ -20,14 +24,32 @@ function loadData() {
 }
 
 
-function writeTable(data) {
+
 
 	// 3.
 	// Make a list of every donation made to "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL"
 	// Do this by looping through the data and writing a new table row (<tr></tr>) for every donation.
-	// Each row should contain of three columns (<td></td>): 
+	// Each row should contain of three columns (<td></td>):
 	// - Contribution Date
 	// - Contributon Information
 	// - Amount
 }
 
+for (i =0; i < donationsData.length; i++) {
+	var date = donationsData[i]["Contribution Date"];
+	var info = donationsData[i]["Contribution Information"];
+	var amount = donationsData[i]["Amount"];
+	var committee = donationsData[i]["Committee"];
+
+	if (Committee === "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL") {
+		console.log(date, info, amount, committee)
+	}
+}
+
+$(".chart").append(
+	"<table class='table-item'>"+
+		"<table class='columns' style='width: "+columnsWidth+"30px'>"+amount+"</div>"+
+		"<table class='rows' style='width: "rowsWidth+"30px'>"+amount+date+info"</div>"+
+	"</table>"
+);
+}
